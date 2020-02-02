b7e2925120477cb63cdbfa5a931668004bfab207
johnliu <johnliutw@hotmail.com>
2020-02-02 11:33:44 +0800

    modify criminal crawler from iframe to web driver

--- criminal_record.py
+++ criminal_record.py
@@ -6,6 +6,7 @@ import urllib.request as urllib2
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup 
+from datetime import datetime
 
 class CriminalRecordCrawler():
 
@@ -27,6 +28,7 @@ class CriminalRecordCrawler():
 
     def run(self):
         try:
+            print('criminal_record start at:' + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
             self.driver.get('''https://law.judicial.gov.tw/FJUD/default.aspx''')
             name = self.driver.find_element_by_id("txtKW")
             name.clear()
@@ -36,26 +38,35 @@ class CriminalRecordCrawler():
             self.driver.find_element_by_id("btnSimpleQry").click()
             time.sleep(1)
           # logging.info("On query result page")
-
             iframe = self.driver.find_element_by_id("iframe-data")
-            response = urllib2.urlopen(iframe.get_attribute('src'))
+            self.driver.get(iframe.get_attribute('src'))
           # logging.info("Get iframe page")
-            iframe_soup = BeautifulSoup(response, 'html.parser')
+            soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+            criminal_rows = soup.select("tr")
+            unfinished = True
+            while(unfinished):
+                soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+                criminal_rows = soup.select("tr")
+                next_btn = soup.select("#hlNext")            
+                # logging.info("Start to enumerate criminal rows")
+                for idx, criminal_row in enumerate(criminal_rows):
+                    if criminal_row.get("class", None) != None or idx == 0:
+                        continue
+                    else:
+                        data = criminal_row.select('td')
+                      # logging.info("Get data for title: " + data[1].text)
+                        title = data[1].text
+                        judge_date = self.parse_date(data[2].text)
+                        reason = data[3].text
+                        self.model.create(title=title, judge_date=judge_date, 
+                                          reason=reason, tenant_id=self.tenant_id)
+                      # logging.info("Criminal Record Crawler Finished")
 
-            criminal_rows = iframe_soup.find_all("tr")
-          # logging.info("Start to enumerate criminal rows")
-            for idx, criminal_row in enumerate(criminal_rows):
-                if criminal_row.get("class", None) != None or idx == 0:
-                    continue
+                if(len(next_btn) > 0):
+                    self.driver.get('''https://law.judicial.gov.tw{}'''.format(next_btn[0]['href']))
                 else:
-                    data = criminal_row.select('td')
-                  # logging.info("Get data for title: " + data[1].text)
-                    title = data[1].text
-                    judge_date = self.parse_date(data[2].text)
-                    reason = data[3].text
-                    self.model.create(title=title, judge_date=judge_date, 
-                                      reason=reason, tenant_id=self.tenant_id)
-                  # logging.info("Criminal Record Crawler Finished")
+                    unfinished = False
+            print('criminal_record end at:' + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
             return True
         except Exception:
           # logging.error("error: " + str(e))
