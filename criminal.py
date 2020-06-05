0416dbd216b27be6347705c863255b6de1872750
johnliu <johnliutw@hotmail.com>
2020-06-05 18:05:32 +0800

    optimize criminal crawler

--- criminal.py
+++ criminal.py
@@ -1,21 +1,15 @@
 import os
-import pytesseract
-import logging 
-import urllib.request as urllib2
 import time
-import pymysql
-import base64
-import requests
 import re
 import traceback
-from io import BytesIO
-from PIL import Image
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup 
 from dotenv import load_dotenv
 from datetime import datetime
-
+from captcha import Captcha
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 load_dotenv()
 
 class CriminalCrawler():
@@ -27,70 +21,23 @@ class CriminalCrawler():
         self.tenant_id = tenant_id
         self.db = db
         self.model = model
-
-      # logging.basicConfig(level=logging.DEBUG, 
-      #              format='%(asctime)s - %(levelname)s : %(message)s', 
-      #              filename='criminal_record.log') 
-
-    def get_captcha(self, driver):
-        img = driver.find_element_by_xpath(".//*[@id='valiCode']")
-        with open('captcha.png', 'wb') as file:
-            file.write(img.screenshot_as_png)   
-      # logging.info("Save Image")  
-        buffer = BytesIO()
-        image = Image.open('captcha.png')
-        image.save(buffer, format="PNG")
-        img_str = base64.b64encode(buffer.getvalue()).decode("utf-8")     
-
-        data = {
-            "clientKey": os.getenv("ANTI_CAPTCHA_KEY"),
-            "task": {
-                "type": "ImageToTextTask",
-                "body": img_str,
-                "phrase":False,
-                "case": True,
-                "numeric": 2,
-                "math": 0,
-                "minLength": 4,
-                "maxLength": 4
-            }
-        }
-
-        r = requests.post("https://api.anti-captcha.com/createTask", json=data)
-        r.raise_for_status()
-        task_id = r.json()['taskId']
-        ret = ""
-        while True:
-            data = {
-                "clientKey": os.getenv("ANTI_CAPTCHA_KEY"),
-                'taskId': task_id
-            }
-            r = requests.post("https://api.anti-captcha.com/getTaskResult", json=data)
-            r.raise_for_status()
-            if r.json()['status'] == 'ready':
-                ret = r.json()['solution']['text']
-                break
-          # logging.info("tring")  
-            time.sleep(5)
-            print('tring')
-        return ret
-
+        self.count = 0
+ 
     def fill_data(self, driver, captcha):
         name = driver.find_element_by_name("CriminalName")
         name.clear()
         name.send_keys(self.name or '')
+
         id_number = driver.find_element_by_name("CriminalIdNo")
         id_number.clear()
         id_number.send_keys(self.id_number or '')
+
         answer = driver.find_element_by_name("WaitValidateCode")
         answer.clear()
         answer.send_keys(captcha)
+
         submit = driver.find_element_by_id("QueryButton")
         submit.click()
-        time.sleep(1)
-        alert = driver.switch_to.alert
-
-        return alert
 
     def parse_date(self, date):
         date_list = date.split('.')
@@ -105,20 +52,15 @@ class CriminalCrawler():
             checkbox.click()
             time.sleep(1)
 
-            captcha = self.get_captcha(self.driver)
-            # logging.info("captcha is " + captcha)  
-            alert = self.fill_data(self.driver, captcha)
-            count = 0
-            while alert.text == '驗證碼不正確!' and count <= 5:
-              # logging.info("retry Submit form")
-              # logging.info("No. " + str(count))
-                time.sleep(1)
+            captcha_parser = Captcha(self.driver, 'valiCode')
+            captcha = captcha_parser.parse() 
+
+            self.fill_data(self.driver, captcha)
+            WebDriverWait(self.driver, 4).until(EC.alert_is_present())
+            alert = self.driver.switch_to.alert
+            while alert.text == '驗證碼不正確':
                 alert.accept() 
-                captcha = self.get_captcha(self.driver)
-              # logging.info("new captcha is " + captcha)
-                alert = self.fill_data(self.driver, captcha)
-                count+=1
-                print(alert.text)
+                raise AttributeError
 
             print(alert.text)
             if(alert.text == '查無資料'):
@@ -129,9 +71,19 @@ class CriminalCrawler():
                 self.model.create(status="abnormal", tenant_id=self.tenant_id)  
             # logging.info("Criminal Record Crawler Finished")             
             return True
+
+        except (AttributeError, TimeoutException):
+            lastCallStack = traceback.format_exc()
+            print(lastCallStack)
+            self.count += 1
+            if self.count < 5:
+                result = self.run()
+                return result
+            else:
+                return False
+
+
         except Exception:
-            self.model.create(status="abnormal", tenant_id=self.tenant_id) 
-            # logging.error("error: " + str(e))
             lastCallStack = traceback.format_exc() #取得Call Stack的最後一筆資料
             print(lastCallStack)
             return False
